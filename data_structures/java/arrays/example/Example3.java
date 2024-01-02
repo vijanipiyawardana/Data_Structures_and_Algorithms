26fcc549191e16073be87519bb59c5c03474d76a
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-03 00:45:50 +0100

    array: Palindrome example  done

--- data_structures/java/arrays/example/Example3.java
+++ data_structures/java/arrays/example/Example3.java
@@ -1,3 +1,30 @@
 public class Example3 {
-    
+
+    // O(N) linear running time complexity
+    // because, consider every single letter exactly once.
+    public boolean solve(String s) {
+
+        int forward = 0;
+        int backward = s.length() - 1;
+
+        while (forward < backward) {
+            if (s.charAt(forward) != s.charAt(backward)) {
+                return false;
+            }
+            forward++;
+            backward--;
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Example3 e3 = new Example3();
+        System.out.println(e3.solve("radar"));
+        System.out.println(e3.solve("rader"));
+        System.out.println(e3.solve("radam"));
+        System.out.println(e3.solve("madar"));
+        System.out.println(e3.solve("acca"));
+    }
 }
