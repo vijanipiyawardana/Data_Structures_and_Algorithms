23197b7097691ade004ef26098e686b18fa242bb
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-04 23:31:32 +0100

    complexity analysis done

--- complexity_analysis/space_complexity/display_cube.py
+++ complexity_analysis/space_complexity/display_cube.py
@@ -9,19 +9,26 @@ def display_cube(items):
 display_cube(items)
 
 # space complexity is O(1), its constant even number of inputs is increased
-
-
 #===============================================================================
 
 def all_cubes(items):
-    result = [];                        # extra memory
+    result = [];                        # extra memory, extra space for each answer
 
     for item in items:
         result.append(pow(item, 3))     # O(n), iterate through list and storing its result, 
                                         # output depends on number of inputs
-
     print(result)
 
 all_cubes(items)
 
 # space complexity is O(n)
+#===============================================================================
+
+def all_cubes_2(items):
+
+    for item in items:
+        result = pow(item, 3)   # O(1), even we do many iterations, we store the result in one variable 
+                                # replacing the value in each iteration, one extra space
+    print(result)
+
+all_cubes_2(items)
